c8f7b287fa4831f0a7f5bf7efc0a37c5337bdefd
D-KG5 <dhruvakoley@gmail.com>
2020-04-13 20:32:09 -0600

    added inotify test program to test gettemp inotify behaviour

--- Dhruva/native_testing/gettemp.sh
+++ Dhruva/native_testing/gettemp.sh
@@ -2,6 +2,18 @@
 # Sensor script to call vcgencmd measure_temp and strip output and save to a log file
 # Author: Dhruva Koley
 
+LOGDIR="log"
+LOGFILE="${LOGDIR}/log.txt"
+
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+
+if !([ -d "${DIR}/${LOGDIR}" ])
+then
+	# build log directory if it doesn't exist
+	cd ${DIR}
+	mkdir -p "${DIR}/${LOGDIR}"
+fi
+
 string="temp=00.0'C"
 # string=$(vgencmd "measure_temp")
 # strip the front and the end of the string to isolate the temperature
